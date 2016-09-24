3ec4c0146cbcb708c5cccdb7fdc31be513a97c1e
osnth01 <osnth01@gmail.com>
2016-09-23 21:41:39 -0400

    create color slider

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 A project to learn Cycle.js
 
 This app shows different combinations of RGB values, which the user can control with sliders.
+
+Learning resource:
+[bmi-typescript](https://github.com/cyclejs/cyclejs/tree/master/examples/bmi-typescript)
---
+++ src/ColorSlider.ts
@@ -0,0 +1,44 @@
+import xs, {Stream, MemoryStream} from 'xstream';
+import {div, span, input, VNode} from '@cycle/dom';
+import {DOMSource} from '@cycle/dom/xstream-typings';
+
+export interface ColorSliderProps {
+  color: string;
+  min: number;
+  initial: number;
+  max: number; 
+}
+
+export type Sources = {
+  DOM: DOMSource,
+  props$: Stream<ColorSliderProps>
+}
+export type Sinks = {
+  DOM: Stream<VNode>,
+  value$: MemoryStream<number>
+}
+
+function ColorSlider(sources: Sources): Sinks {
+  let props$: Stream<ColorSliderProps> = sources.props$;
+  let initialValue$ = props$.map(props => props.initial).take(1);
+  let newValue$ = sources.DOM.select('.slider').events('input')
+    .map(ev => parseInt((<HTMLInputElement> ev.target).value));
+  let value$ = xs.merge(initialValue$, newValue$).remember();
+
+  let vtree$ = xs.combine(props$, value$)
+    .map(([props, value]) =>
+      div('.color-slider', [
+        span('.color', [ props.color + ' ' + value ]),
+        input('.slider', {
+          attrs: {type: 'range', min: props.min, max: props.max, value: value}
+        })
+      ])
+    );
+
+  return {
+    DOM: vtree$,
+    value$: value$
+  };
+}
+
+export default ColorSlider;
